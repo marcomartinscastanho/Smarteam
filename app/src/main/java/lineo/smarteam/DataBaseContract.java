420be1071a8bcb0ae537319b63bafa03984db8df
Marco Martins Castanho <marco.castanho@gmail.com>
2016-09-26 22:55:47 +0100

    Created DataBaseHelper

--- app/src/main/java/lineo/smarteam/DataBaseContract.java
+++ app/src/main/java/lineo/smarteam/DataBaseContract.java
@@ -41,30 +41,4 @@ public final class DataBaseContract {
         public static final String COLUMN_NAME_RESULT = "RESULT";
         public static final String COLUMN_NAME_MATCHDAY_DATE = "MATCHDAY_DATE";
     }
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String INTEGER_TYPE = " INTEGER";
-    private static final String COMMA_SEP = ",";
-    private static final String SQL_CREATE_TABLE_CONFIGURATIONS =
-            "CREATE TABLE " +Configurations.TABLE_NAME +" (" +Configurations.COLUMN_NAME_ATTRIBUTE +" TEXT PRIMARY KEY," +Configurations.COLUMN_NAME_VALUE+TEXT_TYPE +")";
-    private static final String SQL_DELETE_TABLE_CONFIGURATIONS =
-            "DROP TABLE IF EXISTS " +Configurations.TABLE_NAME;
-    private static final String SQL_CREATE_TABLE_TEAMS =
-            "CREATE TABLE " +Teams.TABLE_NAME +" (" +Teams.COLUMN_NAME_ID +" INTEGER PRIMARY KEY," +Teams.COLUMN_NAME_NAME+TEXT_TYPE+COMMA_SEP
-                    +Teams.COLUMN_NAME_NUM_MATCHES+INTEGER_TYPE+COMMA_SEP +Teams.COLUMN_NAME_LAST_MATCH_DATE+INTEGER_TYPE +")";
-    private static final String SQL_DELETE_TABLE_TEAMS =
-            "DROP TABLE IF EXISTS " +Teams.TABLE_NAME;
-    private static final String SQL_CREATE_TABLE_PLAYERS =
-            "CREATE TABLE " +Players.TABLE_NAME +" (" +Players.COLUMN_NAME_ID +" INTEGER PRIMARY KEY," +Players.COLUMN_NAME_NAME+TEXT_TYPE+COMMA_SEP
-                    +Players.COLUMN_NAME_TEAM+INTEGER_TYPE+COMMA_SEP +Players.COLUMN_NAME_WINS+INTEGER_TYPE+COMMA_SEP +Players.COLUMN_NAME_DRAWS+INTEGER_TYPE+COMMA_SEP
-                    +Players.COLUMN_NAME_DEFEATS+INTEGER_TYPE+COMMA_SEP +Players.COLUMN_NAME_MATCHES+INTEGER_TYPE+COMMA_SEP +Players.COLUMN_NAME_MATCHES_AFTER_DEBUT+INTEGER_TYPE+COMMA_SEP
-                    +Players.COLUMN_NAME_WIN_PERCENTAGE+INTEGER_TYPE+COMMA_SEP +Players.COLUMN_NAME_SCORE+INTEGER_TYPE +")";
-    private static final String SQL_DELETE_TABLE_PLAYERS =
-            "DROP TABLE IF EXISTS " +Players.TABLE_NAME;
-    private static final String SQL_CREATE_TABLE_INDIVIDUAL_RESULTS =
-            "CREATE TABLE " +IndividualResults.TABLE_NAME +" (" +IndividualResults.COLUMN_NAME_PLAYER_ID+INTEGER_TYPE+COMMA_SEP
-                    +IndividualResults.COLUMN_NAME_TEAM_ID+INTEGER_TYPE+COMMA_SEP +IndividualResults.COLUMN_NAME_MATCHDAY+INTEGER_TYPE+COMMA_SEP
-                    +IndividualResults.COLUMN_NAME_RESULT+TEXT_TYPE+COMMA_SEP +IndividualResults.COLUMN_NAME_MATCHDAY_DATE+INTEGER_TYPE+COMMA_SEP
-                    +" PRIMARY KEY (" +IndividualResults.COLUMN_NAME_PLAYER_ID+COMMA_SEP +IndividualResults.COLUMN_NAME_MATCHDAY +")" +")";
-    private static final String SQL_DELETE_TABLE_INDIVIDUAL_RESULTS =
-            "DROP TABLE IF EXISTS " +IndividualResults.TABLE_NAME;
 }
