4f1796929035b53dfdc7853a615dbd7695de4626
Marco Martins Castanho <marco.castanho@gmail.com>
2016-10-27 03:31:59 +0100

    Created MyApplication for use of Global Toast

---
+++ app/src/main/java/lineo/smarteam/MyApplication.java
@@ -0,0 +1,29 @@
+package lineo.smarteam;
+
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by marco on 23/10/2016.
+ */
+public class MyApplication extends Application {
+    private static final String TAG = "AppGlobal";
+    private Toast toast;
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public void showToast(Context context, String string){
+        if(toast == null){
+            toast = new Toast(context);
+        }
+        else{
+            toast.cancel();
+        }
+        toast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
