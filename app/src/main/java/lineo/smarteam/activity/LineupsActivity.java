ca43a5590dfb79e9b8d6eee2df6907a7326525de
Marco Martins Castanho <marco.castanho@gmail.com>
2017-10-07 20:05:00 +0100

    Fixed feeze in LineupsActivity when number of players is high and added a progress bar.

--- app/src/main/java/lineo/smarteam/activity/LineupsActivity.java
+++ app/src/main/java/lineo/smarteam/activity/LineupsActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.ShareActionProvider;
 
 import java.util.ArrayList;
@@ -29,6 +31,7 @@ public class LineupsActivity extends Activity implements View.OnClickListener {
 
     private Integer teamId;
     private ArrayList<Integer> selectedPlayersIndexList = new ArrayList<>();
+    private Integer lineupsCalculationComplexity;
 
     private String[] lineupHome;
     private String[] lineupAway;
@@ -40,22 +43,20 @@ public class LineupsActivity extends Activity implements View.OnClickListener {
         context=this;
         getIntentExtras();
         setActionBarTitle();
-        //setContentView(R.layout.loading);
-        generateLineups(); //FIXME: this might take a while, screen cannot be empty in the meanwhile
-        //TODO: use the same logic as in the Splash Screen
         setLayout();
-
+        new GenerateLineups().execute(lineupsCalculationComplexity);
     }
 
     private void setLayout(){
-        //findViewById(R.id.loadingPanel).setVisibility(View.GONE);
         setContentView(R.layout.activity_lineups);
+    }
+
+    private void inflateLists(){
         ListView listHome = findViewById(R.id.listViewTeamHome);
         ListView listAway = findViewById(R.id.listViewTeamAway);
         listHome.setAdapter(new ArrayAdapter<>(context, R.layout.lineups_line_black, lineupHome));
         listAway.setAdapter(new ArrayAdapter<>(context, R.layout.lineups_line_white, lineupAway));
         //TODO: put both lists centered vertically
-
     }
 
     private void getIntentExtras(){
@@ -72,6 +73,7 @@ public class LineupsActivity extends Activity implements View.OnClickListener {
             MyApplication.showToast(context, getResources().getString(R.string.toastFailedToLoadTeam));
             finish();
         }
+        lineupsCalculationComplexity = 2 * (int) Math.exp(2 * selectedPlayersIndexList.size() / 3);
     }
 
     private void setActionBarTitle(){
@@ -117,106 +119,138 @@ public class LineupsActivity extends Activity implements View.OnClickListener {
         return true;
     }
 
-    private void generateLineups() {
-        class Player{
-            private Integer playerId;
-            private String playerName;
-            private Double score;
 
-            Player(Integer playerId, String playerName, Double score){
-                this.playerId = playerId;
-                this.playerName = playerName;
-                this.score = score;
-            }
-
-            public Integer getId() {
-                return playerId;
-            }
-
-            public String getName() {
-                return playerName;
-            }
-
-            Double getScore() {
-                return score;
-            }
+    private class GenerateLineups extends AsyncTask<Integer, Integer, String> {
+        ProgressBar progressBar;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar = findViewById(R.id.progressBar);
+            progressBar.setMax(lineupsCalculationComplexity);
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(0);
         }
 
-        class LineupGenerator{
-            private Integer maxPlayersPerTeam;
-            private Double provisionalDiff;
-            private ArrayList<Player> selectedPlayers;
-            private ArrayList<Player> p1;
-            private ArrayList<Player> p2;
-            private ArrayList<Player> res1;
-            private ArrayList<Player> res2;
+        @Override
+        protected String doInBackground(Integer... complexity) {
+            class Player{
+                private Integer playerId;
+                private String playerName;
+                private Double score;
 
-            private LineupGenerator(ArrayList<Integer> selectedPlayersIndexList){
-                maxPlayersPerTeam = (selectedPlayersIndexList.size()+1)/2;
-                provisionalDiff =  Integer.valueOf(100*selectedPlayersIndexList.size()).doubleValue();
-                selectedPlayers = new ArrayList<>(selectedPlayersIndexList.size());
-                Cursor c = MyApplication.db.getLineupInfoByTeamId(teamId);
-                Integer cursorCount=0;
-                if(c.moveToFirst()){
-                    do{
-                        if(selectedPlayersIndexList.contains(cursorCount)){
-                            selectedPlayers.add(new Player(c.getInt(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_ID)), c.getString(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_NAME)), c.getDouble(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_SCORE))));
+                Player(Integer playerId, String playerName, Double score){
+                    this.playerId = playerId;
+                    this.playerName = playerName;
+                    this.score = score;
+                }
+
+                public Integer getId() {
+                    return playerId;
+                }
+
+                public String getName() {
+                    return playerName;
+                }
+
+                Double getScore() {
+                    return score;
+                }
+            }
+
+            class LineupGenerator{
+                private Integer progress=0;
+                private Integer maxPlayersPerTeam;
+                private Double provisionalDiff;
+                private ArrayList<Player> selectedPlayers;
+                private ArrayList<Player> p1;
+                private ArrayList<Player> p2;
+                private ArrayList<Player> res1;
+                private ArrayList<Player> res2;
+
+                private LineupGenerator(ArrayList<Integer> selectedPlayersIndexList){
+                    maxPlayersPerTeam = (selectedPlayersIndexList.size()+1)/2;
+                    provisionalDiff =  Integer.valueOf(100*selectedPlayersIndexList.size()).doubleValue();
+                    selectedPlayers = new ArrayList<>(selectedPlayersIndexList.size());
+                    Cursor c = MyApplication.db.getLineupInfoByTeamId(teamId);
+                    Integer cursorCount=0;
+                    if(c.moveToFirst()){
+                        do{
+                            if(selectedPlayersIndexList.contains(cursorCount)){
+                                selectedPlayers.add(new Player(c.getInt(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_ID)), c.getString(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_NAME)), c.getDouble(c.getColumnIndexOrThrow(DataBase.PLAYERS_COLUMN_SCORE))));
+                            }
+                            ++cursorCount;
                         }
-                        ++cursorCount;
+                        while(c.moveToNext());
                     }
-                    while(c.moveToNext());
+                    p1 = new ArrayList<>(maxPlayersPerTeam+1);
+                    p2 = new ArrayList<>(maxPlayersPerTeam+1);
+                    res1 = new ArrayList<>(maxPlayersPerTeam);
+                    res2 = new ArrayList<>(maxPlayersPerTeam);
                 }
-                p1 = new ArrayList<>(maxPlayersPerTeam+1);
-                p2 = new ArrayList<>(maxPlayersPerTeam+1);
-                res1 = new ArrayList<>(maxPlayersPerTeam);
-                res2 = new ArrayList<>(maxPlayersPerTeam);
+
+                private void partition(Integer i, Double sum1, Double sum2){
+                    ++progress;
+                    publishProgress(progress);
+                    if(p1.size()>maxPlayersPerTeam)
+                        return;
+                    if(p2.size()>maxPlayersPerTeam)
+                        return;
+                    if(i.equals(selectedPlayers.size())){
+                        Double newDiff = Math.abs(sum1 - sum2);
+                        if(newDiff < provisionalDiff){
+                            res1 = new ArrayList<>(p1);
+                            res2 = new ArrayList<>(p2);
+                            provisionalDiff = newDiff;
+                        }
+                        return;
+                    }
+                    p1.add(selectedPlayers.get(i));
+                    partition(i+1, sum1 + selectedPlayers.get(i).getScore(), sum2);
+                    if(provisionalDiff.equals(0.0))
+                        return;
+                    p1.remove(p1.size()-1);
+                    p2.add(selectedPlayers.get(i));
+                    partition(i+1, sum1, sum2 + selectedPlayers.get(i).getScore());
+                    if(provisionalDiff.equals(0.0))
+                        return;
+                    p2.remove(p2.size()-1);
+                }
             }
 
-            private void partition(Integer i, Double sum1, Double sum2){
-                if(p1.size()>maxPlayersPerTeam)
-                    return;
-                if(p2.size()>maxPlayersPerTeam)
-                    return;
-                if(i.equals(selectedPlayers.size())){
-                    Double newDiff = Math.abs(sum1 - sum2);
-                    if(newDiff < provisionalDiff){
-                        res1 = new ArrayList<>(p1);
-                        res2 = new ArrayList<>(p2);
-                        provisionalDiff = newDiff;
-                    }
-                    return;
-                }
-                p1.add(selectedPlayers.get(i));
-                partition(i+1, sum1 + selectedPlayers.get(i).getScore(), sum2);
-                if(provisionalDiff.equals(0.0))
-                    return;
-                p1.remove(p1.size()-1);
-                p2.add(selectedPlayers.get(i));
-                partition(i+1, sum1, sum2 + selectedPlayers.get(i).getScore());
-                if(provisionalDiff.equals(0.0))
-                    return;
-                p2.remove(p2.size()-1);
+            LineupGenerator lineupGen = new LineupGenerator(selectedPlayersIndexList);
+            lineupGen.partition(0, 0.0, 0.0);   //Magic Happens Here
+            lineupHome = new String[lineupGen.res1.size()];
+            lineupAway = new String[lineupGen.res2.size()];
+            Double strengthHome=0.0;
+            Double strengthAway=0.0;
+            Integer i=0;
+            for(Player p : lineupGen.res1){
+                lineupHome[i] = p.getName();
+                strengthHome += p.getScore()*100;
+                ++i;
             }
+            i=0;
+            for(Player p : lineupGen.res2){
+                lineupAway[i] = p.getName();
+                strengthAway += p.getScore()*100;
+                ++i;
+            }
+            Log.i(TAG, "generateLineups() "+strengthHome+ " vs "+strengthAway);
+            return null;
         }
 
-        LineupGenerator lineupGen = new LineupGenerator(selectedPlayersIndexList);
-        lineupGen.partition(0, 0.0, 0.0);   //Magic Happens Here
-        lineupHome = new String[lineupGen.res1.size()];
-        lineupAway = new String[lineupGen.res2.size()];
-        Double strengthHome=0.0;
-        Double strengthAway=0.0;
-        Integer i=0;
-        for(Player p : lineupGen.res1){
-            lineupHome[i] = p.getName();
-            strengthHome += p.getScore()*100;
-            ++i;
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            inflateLists();
+            progressBar.setVisibility(View.GONE);
         }
-        i=0;
-        for(Player p : lineupGen.res2){
-            lineupAway[i] = p.getName();
-            strengthAway += p.getScore()*100;
-            ++i;
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
         }
-        Log.i(TAG, "generateLineups() "+strengthHome+ " vs "+strengthAway);
     }
+
 }
